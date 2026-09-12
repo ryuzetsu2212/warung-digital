6fdc93b5d9c8da7425814ca028558f06bc64f6e7
ryuzetsu1 <cityandspectre@gmail.com>
2026-09-13 01:36:37 +0700

    remove redundant use PDO statement causing boot warning

--- config/database.php
+++ config/database.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Str;
 
-use Illuminate\Support\Facades\DB;
-use PDO;
-
 return [
 
     /*
@@ -98,9 +95,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
-            'options' => extension_loaded('pdo_pgsql') ? [
-                PDO::ATTR_EMULATE_PREPARES => false,
-            ] : [],
         ],
 
         'sqlsrv' => [
