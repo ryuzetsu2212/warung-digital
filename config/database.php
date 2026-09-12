0fb1a484f85051a8d4c265169f3f14a47affca4a
ryuzetsu1 <cityandspectre@gmail.com>
2026-09-13 00:47:01 +0700

    support transaction pooler prepared statements

--- config/database.php
+++ config/database.php
@@ -94,7 +94,8 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => extension_loaded('pdo_pgsql') ? [PDO::ATTR_EMULATE_PREPARES => true] : [],
         ],
 
         'sqlsrv' => [
